2c751bfa3d2bd38c3093d9c7d1b13133e6c34ed0
GGBOYwork <809767100@qq.com>
2019-04-14 13:44:59 +0800

    Update pointer.c

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -1,1 +1,17 @@
 #include<stdio.h>
+ void swap(int *x, int *y)
+{
+	int t;
+	t = *x;
+	*x= *y;
+	*y=  t;
+}
+ int main(void)
+{
+	int a, b;
+	a = 8;
+	b = 4;
+	swap(&a, &b);
+	printf("%d,%d\n",a,b );
+	return 0;
+}
